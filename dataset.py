b27e9654e030dc14f3264b2db2e7cd9ee5f3a194
Lea Gomis <lea.187@hotmail.fr>
2025-02-25 02:50:01 +0100

    ajout d'un scatterplot à la viz

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+ #Création du dataset factice
 fake = Faker()
 
 num_products = 50
@@ -53,12 +54,14 @@ final_df = sales_df.merge(products_df, on="product_id", how="left")
 
 final_df["date"] = pd.to_datetime(final_df["date"])
 
+#final_df.to_csv("sales_data.csv", index=False) pour enregistrer le fichier
+
+#Histogramme du stock et courbes de tendances sur 7 jours puis 14 jours
 plt.figure(figsize=(8,5))
 sns.histplot(final_df["stock_quantity"], bins=20, kde=True, color="royalblue")
 plt.title("Répartition des stocks des produits")
 plt.xlabel("Quantité en stock")
 plt.ylabel("Nombre de produits")
-#plt.show()
 
 daily_sales = final_df.groupby("date")["units_sold"].sum().reset_index()
 daily_sales["moving_avg"] = daily_sales["units_sold"].rolling(window=7).mean()
@@ -72,7 +75,24 @@ plt.xlabel("Date")
 plt.ylabel("Nombre d'unités vendues")
 plt.legend()
 plt.xticks(rotation=45)
+
+daily_sales["moving_avg_14"] = daily_sales["units_sold"].rolling(window=14).mean()
+
+plt.figure(figsize=(12,6))
+sns.lineplot(x="date", y="units_sold", data=daily_sales, label="Ventes Journalières", color="royalblue")
+sns.lineplot(x="date", y="moving_avg_14", data=daily_sales, label="Moyenne mobile (14 jours)", color="red")
+
+plt.title("Évolution des ventes avec lissage sur 14 jours")
+plt.xlabel("Date")
+plt.ylabel("Nombre d'unités vendues")
+plt.legend()
+plt.xticks(rotation=45)
+
+#Scatterplot relation entre le stock et les ventes
+plt.figure(figsize=(8,5))
+sns.scatterplot(x=final_df["stock_quantity"], y=final_df["units_sold"], alpha=0.5, color="royalblue")
+
+plt.title("Relation entre le stock et les ventes")
+plt.xlabel("Stock restant")
+plt.ylabel("Unités vendues")
 plt.show()
-
-#final_df.to_csv("sales_data.csv", index=False) pour enregistrer le fichier
-
