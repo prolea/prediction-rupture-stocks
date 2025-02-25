8c35700f4632f8d71c46c9f120cd9757453e0343
Lea Gomis <lea.187@hotmail.fr>
2025-02-25 02:17:18 +0100

    Ajout visualisation sur fichier python

--- dataset.py
+++ dataset.py
@@ -3,6 +3,8 @@ import numpy as np
 from faker import Faker
 import random
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 fake = Faker()
 
@@ -49,6 +51,28 @@ sales_df = pd.DataFrame(sales_data)
 
 final_df = sales_df.merge(products_df, on="product_id", how="left")
 
-print(final_df.head())
+final_df["date"] = pd.to_datetime(final_df["date"])
 
-final_df.to_csv("sales_data.csv", index=False)
+plt.figure(figsize=(8,5))
+sns.histplot(final_df["stock_quantity"], bins=20, kde=True, color="royalblue")
+plt.title("Répartition des stocks des produits")
+plt.xlabel("Quantité en stock")
+plt.ylabel("Nombre de produits")
+#plt.show()
+
+daily_sales = final_df.groupby("date")["units_sold"].sum().reset_index()
+daily_sales["moving_avg"] = daily_sales["units_sold"].rolling(window=7).mean()
+
+plt.figure(figsize=(12,6))
+sns.lineplot(x="date", y="units_sold", data=daily_sales, label="Ventes journalières", color="royalblue")
+sns.lineplot(x="date", y="moving_avg", data=daily_sales, label="Moyenne mobile (7 jours)", color="red")
+
+plt.title("Évolution des ventes journalières")
+plt.xlabel("Date")
+plt.ylabel("Nombre d'unités vendues")
+plt.legend()
+plt.xticks(rotation=45)
+plt.show()
+
+#final_df.to_csv("sales_data.csv", index=False) pour enregistrer le fichier
+
